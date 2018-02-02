38440e0280172bc90782042ac16a505940e1811d
bhawesh96 <bhansalibhawesh@yahoo.com>
2018-02-03 02:11:09 +0530

    empty state, ellipsis added

--- src/Components/HomeComponent/NotificationsContainer.js
+++ src/Components/HomeComponent/NotificationsContainer.js
@@ -7,7 +7,7 @@ import CommunicationChatBubble from 'material-ui/svg-icons/communication/chat-bu
 
 import {hashHistory} from 'react-router'
 import {firebaseAuth} from '../../firebaseConfig'
-
+import Parser from 'html-react-parser'
 import {connect} from 'react-redux'
 
 import axios from 'axios'
@@ -45,6 +45,7 @@ class NotificationsContainer extends Component {
         var x = data.new[0]
         people.push(x)
         scope.setState({people})
+        console.log(data)
       })
         .then(successCallback, failCallback);
     })
@@ -53,21 +54,32 @@ class NotificationsContainer extends Component {
     })
   }
 
+  // style={{whiteSpace: 'nowrap', overflow: 'hidden', maxWidth: '30%', position: 'absolute', textOverflow: 'ellipsis', padding: 'inherit'}}
+
   render() {
+    const Text = (props) => {
+      return (
+      <span style={{whiteSpace: 'nowrap', overflow: 'hidden', maxWidth: '50%', position: 'absolute', textOverflow: 'ellipsis', height: 30}}> {Parser(props.content)}</span>
+      )
+    }
     return (
       
         <List style={{width: '100%', height: 400, overflowY: 'scroll'}}>
           {this.state.people && Object.values(this.state.people.reverse()).map((person, index) => (
               <ListItem
                 key={index}
-                primaryText={person.name + ' ' + person.verb + 'ed ' + person.content}
-                leftAvatar={<Avatar src={person.pp_url} />}
+                primaryText={<div><span>{person.name} {person.verb}d </span> your post - <Text content={person.content}/></div>}
+                leftAvatar={<Avatar style={{marginTop: 10}} src={person.pp_url} />}
               />
             ))
           }
 
-          { this.state.people.length < 1 && <img src="https://cdn.dribbble.com/users/111395/screenshots/2735335/empty_state_icons-22.png" style={{width: '100%', marginTop: 60}} /> }
-
+          { this.state.people.length < 1 && <div><img src="https://cdn.dribbble.com/users/111395/screenshots/2735335/empty_state_icons-22.png" style={{width: '100%', marginTop: 60}} /><p style={{width: '100%', textAlign: 'center'}}>No new notifications</p></div> }
+          {/*<ListItem
+           key={1}
+           primaryText={<div><span>{person.name}</span><span>{person.verb}ed</span> your post <Text /></div>}
+           leftAvatar={<Avatar src={this.props.user.photoURL} />} />*/}
+          
         </List>
 
     );
